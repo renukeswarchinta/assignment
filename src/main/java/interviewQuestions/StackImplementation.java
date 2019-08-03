1bda5d8a6557cc369a2b4b1bb15fc51b34e27c14
renukeswarchinta <41563424+renukeswarchinta@users.noreply.github.com>
2019-08-03 13:46:44 +0530

    Stack Implementation

---
+++ src/main/java/interviewQuestions/StackImplementation.java
@@ -0,0 +1,53 @@
+package interviewQuestions;
+
+public class StackImplementation {
+
+	public static void main(String[] args) {
+		Stack stack = new Stack(10);
+		stack.push(1);stack.push(10);stack.push(2);
+		
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.peek());
+	}
+}
+
+
+class Stack{
+	int array[];
+	int top;
+	int capacity ;
+	Stack(int size){
+		array = new int[size];
+		top = -1;
+		capacity = size;
+	}
+	public void push(int x) {
+		if(isFull()) {
+			System.out.println("Stack is full");
+			System.exit(1);
+		}
+		System.out.println("Pushing element on the top "+x);
+		array[++top] = x;
+	}
+	private boolean isFull() {
+		return capacity == top -1;
+	}
+	public int pop() {
+		if(isEmpty()) {
+			System.out.println("This is empty");
+			System.exit(1);
+		}
+		System.out.println("Poping element from the top ");
+		return array[top--];
+	}
+	private boolean isEmpty() {
+		return top == -1;
+	}
+	public int peek() {
+		return array[top];
+	}
+	public int size() {
+		return top+1;
+	}
+}
